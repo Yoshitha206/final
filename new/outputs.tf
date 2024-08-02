b9a599ca15e967f3b9e04fa5e692aaf88fb8c507
Yoshitha206 <yoshithaa35@gmail.com>
2024-08-02 17:56:59 +0530

    Update outputs.tf

--- new/outputs.tf
+++ new/outputs.tf
@@ -1,6 +1,6 @@
-output "sql_mi_id" {
-  value = azurerm_mssql_managed_instance.sql_mi.id
-}
+#output "sql_mi_id" {
+ # value = azurerm_mssql_managed_instance.sql_mi.id
+#}
 
 output "app_service_id" {
   value = azurerm_windows_web_app.appservice_app.id
