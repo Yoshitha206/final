759c5567a58c278b122c4c9ac1c052c98097d093
Yoshitha206 <yoshithaa35@gmail.com>
2024-08-06 15:50:26 +0530

    Update outputs.tf

--- new/outputs.tf
+++ new/outputs.tf
@@ -5,3 +5,10 @@
 output "app_service_id" {
   value = azurerm_windows_web_app.appservice_app.id
 }
+output "sql_server_id" {
+  value = azurerm_sql_server.sql_server.id
+}
+
+output "sql_database_id" {
+  value = azurerm_sql_database.sql_database.id
+}
